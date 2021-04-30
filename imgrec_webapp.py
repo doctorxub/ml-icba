205c38a19ea2223cf5224803d667207135a89737
Alexi Akl <alexiakl@gmail.com>
2021-04-30 12:07:57 +0200

    wip

--- imgrec_webapp.py
+++ imgrec_webapp.py
@@ -5,7 +5,7 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from tensorflow.compat.v1.keras.backend import set_session
 from skimage.transform import resize
 import matplotlib.pyplot as plt
