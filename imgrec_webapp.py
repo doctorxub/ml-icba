164ea3cc566e2651aa6dd86b90f455cfbdc36f7c
Alexi Akl <alexiakl@gmail.com>
2021-04-30 12:04:55 +0200

    wip

--- imgrec_webapp.py
+++ imgrec_webapp.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 from keras.models import load_model
-from keras.backend import set_session
+from tensorflow.compat.v1.keras.backend import set_session
 from skimage.transform import resize
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -14,7 +14,7 @@ import numpy as np
 
 print("Loading model")
 global sess
-sess = tf.Session()
+sess = tf.compat.v1.Session()
 set_session(sess)
 global model
 model = load_model('my_cifar10_model.h5')
