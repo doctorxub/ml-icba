980e84cd235a7f2ab19cea90499ae1a99fdeafc9
Alexi Akl <alexiakl@gmail.com>
2021-04-30 13:58:04 +0200

    wip

--- webapp.py
+++ webapp.py
@@ -68,7 +68,7 @@ def prediction(filename):
     #Step 1
     my_image = plt.imread(os.path.join('uploads', filename))
     #Step 2
-    my_image_re = resize(my_image, (32,32,3))
+    my_image_re = resize(my_image, (224,224))
 
     #Step 3
     with graph.as_default():
