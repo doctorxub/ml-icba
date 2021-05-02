8fc13a1cc0c22d451c3faaf7b3bdef7376a0fe76
Alexi Akl <alexiakl@gmail.com>
2021-05-02 16:24:06 +0200

    wip

--- webapp.py
+++ webapp.py
@@ -1,33 +1,13 @@
 import os
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
-
-
-app = Flask(__name__)
-
-from tensorflow.keras.models import load_model
-from skimage.transform import resize
-import matplotlib.pyplot as plt
-import tensorflow as tf
 import numpy as np
-
-
 from tensorflow.keras.layers import Dense,GlobalAveragePooling2D
-from tensorflow.keras.applications import InceptionResNetV2, VGG16, ResNet50
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam, SGD
-from tensorflow.keras.utils import to_categorical
 import cv2
 
-
-global model_to_use
-global classes
-global base_model
-global x
-global preds
-global predictions
+app = Flask(__name__)
 
 classes = ['0', '1', '2', '3', '4', '5']
 predictions = [
@@ -56,13 +36,6 @@ model=Model(inputs=base_model.input, outputs=preds)
 model_path = 'model-epoch-002-valacc-0.976103.h5'
 model.load_weights(model_path)
 
-print("Loading model")
-global sess
-sess = tf.compat.v1.Session()
-tf.compat.v1.keras.backend.set_session(sess)
-global graph
-graph = tf.compat.v1.get_default_graph()
-
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
     if request.method == 'POST':
@@ -80,7 +53,6 @@ def prediction(filename):
     classes = model.predict(img, batch_size=1)
     max_index = np.argmax(classes, axis=1)
 
-    #Step 5
     return render_template('result.html', predictions=predictions[max_index[0]])
 
 app.run(host='0.0.0.0', port=2000)
