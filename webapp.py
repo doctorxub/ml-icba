e8a836e4fae8a9ecf84ad6f311d57172e38d9630
Alexi Akl <alexiakl@gmail.com>
2022-02-09 08:37:13 +0100

    feat: pinecones model

--- webapp.py
+++ webapp.py
@@ -2,6 +2,11 @@ import os
 from flask import Flask, request, redirect, url_for, render_template, jsonify
 from werkzeug.utils import secure_filename
 import numpy as np
+from mrcnn import model as modellib, utils, visualize
+from mrcnn.config import Config
+import matplotlib.pyplot as plt
+import json
+import skimage
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.models import Model
@@ -114,10 +119,50 @@ def icba_diseases():
 # ----------------------------------------------------------
 # CTFC
 # ----------------------------------------------------------
-# TODO set up ctfc model
+tag_name = 'conepines'
+
+class PineConfig(Config):
+    NAME = tag_name
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    NUM_CLASSES = 1 + 1  # Background + 1 (Conepine)
+    IMAGE_RESIZE_MODE = "crop"
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+    IMAGE_MIN_SCALE = 2.0
+    DETECTION_MIN_CONFIDENCE = 0
+    STEPS_PER_EPOCH = 500
+    VALIDATION_STEPS = 5
+    BACKBONE = 'resnet50'
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+    TRAIN_ROIS_PER_IMAGE = 128
+    MAX_GT_INSTANCES = 500
+    POST_NMS_ROIS_INFERENCE = 2000 
+    POST_NMS_ROIS_TRAINING = 1000
+    RPN_NMS_THRESHOLD = 0.9
+    RPN_TRAIN_ANCHORS_PER_IMAGE = 64
+    MEAN_PIXEL = np.array([43.53, 39.56, 48.22])
+    USE_MINI_MASK = True
+    MINI_MASK_SHAPE = (56, 56) # (height, width) of the mini-mask -It doesn't change-
+    DETECTION_MAX_INSTANCES = 1000    
+
+config = PineConfig()
+config.display()
+
+class InferenceConfig(PineConfig):
+    IMAGE_RESIZE_MODE = "pad64"
+    RPN_NMS_THRESHOLD = 0.7
+
+inference_config = InferenceConfig()
+inference_config.display()
+
+ctfc_model_path = 'static/models/ctfc/model_v1.h5'
+ctfc_data_dir = 'data'
+model = modellib.MaskRCNN(mode="inference", config=inference_config, model_dir=ctfc_data_dir)
+model.load_weights(ctfc_model_path, by_name=True)
 
 def ctfc_predict(filename):
-    result = {'index': -1, 'confidence': 0, 'errorMsg': ''}
+    result = {'index': -1, 'errorMsg': ''}
 
     if not filename:
         result['errorMsg'] = "Please select a file to classify"
@@ -132,10 +177,9 @@ def ctfc_predict(filename):
             result['errorMsg'] = "Unsupported format"
             return result
 
-        # TODO ctfc model prediction algorithm
-        
+        result = model.detect([img], verbose=0)[0]
+        print(result)
         result['index'] = 1
-        result['confidence'] = 99
         os.remove(file_path)
     
     if result['index'] < 0:
@@ -157,15 +201,10 @@ def render_ctfc_main_page():
 def render_ctfc_predict(filename):
     result = ctfc_predict(filename)
     i = result.get('index')
-    c = result.get('confidence')
 
     if i < 0:
         return render_template('ctfc/error.html', message=result.get('errorMsg'))
-    return render_template('ctfc/result.html', predictions=ctfc_html_predictions[i], confidence=c)
-
-@app.route('/ctfc/diseases')
-def render_ctfc_diseases():
-    return render_template('ctfc/diseases.html', predictions=ctfc_html_predictions)
+    return render_template('ctfc/result.html', predictions=result)
 
 @app.route('/api/ctfc/upload', methods=['POST'])
 def ctfc_upload_image():
@@ -180,13 +219,8 @@ def ctfc_upload_image():
 def ctfc_api_predict(filename):
     result = ctfc_predict(filename)
     i = result.get('index')
-    c = result.get('confidence')
 
     if i < 0:
         return jsonify(success=0, message=result.get('errorMsg'))
-    return jsonify(success=1, disease=ctfc_diseases_list[i], confidence=c)
-
-@app.route('/api/ctfc/diseases')
-def ctfc_diseases():
-    return jsonify(success=1, diseases=ctfc_diseases_list)
+    return jsonify(success=1, result=result)
 # ----------------------------------------------------------
